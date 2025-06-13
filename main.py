8dcced74b42309bb5d87da517ad92cc87d1106c8
ARUNPRASAD89 <arunthemercury@gmail.com>
2025-06-13 16:39:17 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from login_window import LoginWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = LoginWindow()
-    window.show()
-    sys.exit(app.exec_())
